ff30c62915b7162d152c0dd30000fb4fbac7e84f
kkunh97@naver.com <kkunh97@naver.com>
2024-02-18 10:56:59 +0900

    # commit Test

--- DyanamicAllocation/main.cpp
+++ DyanamicAllocation/main.cpp
@@ -1,14 +1,9 @@
-//
-//  main.cpp
-//  DyanamicAllocation
-//
-//  Created by 김건혁 on 2/18/24.
-//
-
 #include <iostream>
+using namespace std;
+
 
 int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
-    return 0;
+    int num = 10;
+    cout << num << endl;
+    cout << "TEST CASE" << endl;
 }
