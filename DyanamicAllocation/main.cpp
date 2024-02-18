7d2d13445797e4a26dda5110d0965acfd906825e
kkunh97@naver.com <kkunh97@naver.com>
2024-02-18 10:46:43 +0900

    Initial Commit

---
+++ DyanamicAllocation/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  DyanamicAllocation
+//
+//  Created by 김건혁 on 2/18/24.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
